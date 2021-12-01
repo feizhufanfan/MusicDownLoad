618e907dd4b515e001e54bd7068d2551b67d261f
du <1009963012@qq.com>
2021-12-02 00:22:14 +0800

    更新下载资源异常的问题

--- README.md
+++
@@ -1,2 +0,0 @@
- # 适用于LISTEN1 网易音乐的下载
- # user for Listen1 Download wangyi music 
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 from sys import argv
 import re
 import requests
-from time import time
+import time
 from multiprocessing.pool import ThreadPool
 def GetJson(filePAth):
     file_Object=open(filePAth,"r",encoding="UTF-8")
@@ -36,24 +36,58 @@ def Url_response(url):
 
 def GetDownLoadList(path,data):
     for value in data:
-        id,title,artist=value["id"],value["title"],value["artist"]
-        id=re.findall("\d+",id)
-        url_data="http://music.163.com/song/media/outer/url?id="+id[0]+".mp3"
-        r = requests.get(url_data, stream=True)
-        title=title.replace("/","")
-        artist=artist.replace("/","")
 
+        id,title,artist=value["id"],value["title"],value["artist"]
+        title = title.replace("/", "")
+        title = title.replace("\"", "\'")
+        print('下载[{file}]'.format(file=title))
+        # char_index=0
+        # for char in title:
+        #     if not (char in range(128)):
+        #         title[char_index]=""
+        #     char_index+=1
+        # artist = artist.replace("/", "")
+        # artist = artist.replace("\"", "\'")
+        # for char in artist:
+        #     if not (char in range(128)):
+        #         artist[char_index] = ""
+        #     char_index += 1
+        size = 0  # 初始化已下载大小
+        chunk_size = 1024  # 每次下载的数据大小
+        id=re.findall("\d+",id)
+        filepath = path + "/" + artist + "-" +  title+ ".mp3"
         if not os.path.exists(path):
             os.makedirs(path)
-        filepath=path+"/"+title+"-"+artist+".mp3"
+        if os.path.exists(filepath):
+            continue
+        url_data="http://music.163.com/song/media/outer/url?id="+id[0]+".mp3"
+        start = time.time()  # 下载开始时间
+        r = requests.get(url_data, stream=True)
+        if r.status_code != 200:
+            print("资源地址异常")
+            continue
+
+        if not "Content-Length" in r.headers.keys():
+            print("资源地址异常")
+            continue
+
+        content_size = int(r.headers['Content-Length'])  # 下载文件总大小
+
+
+        print('开始下载[{file}]:{size:.2f} MB'.format(file=title,size=content_size / chunk_size / 1024))  # 开始下载，显示下载文件大小
+
         with open(filepath, "wb") as f:
-            for ch in r:
-                f.write(ch)
+            for data in r.iter_content(chunk_size = chunk_size):
+                f.write(data)
+                size+=len(data)
+                print(
+                    '\r' + '[下载进度]:%s%.2f%%' % ('>' * int(size * 50 / content_size), float(size / content_size * 100)),
+                    end=' ')
         f.close()
+        end = time.time()  # 下载结束时间
+        print('下载完成times: %.2f秒' % (end - start))  # 输出下载用时时间
 
-def Downloads(filename,path,url):
 
-    pass
 
 def main(filePath):
     # Use a breakpoint in the code line below to debug your script.
